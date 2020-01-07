be051d7dceeba64f15e815bc83b76f8c4b01111d
Guilherme Mulato <guilherme_mulato@hotmail.com>
2020-01-06 22:55:02 -0300

    Projeto Stage Final

---
+++ Aluno/Aluno.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Aluno
+{
+    public class Aluno
+    {
+
+        public string Nome;
+        public double n1, n2, n3;
+
+        public double NotaFinal()
+        {
+            return n1 + n2 + n3;
+        }
+
+    }
+}
---
+++ Aluno/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace Aluno
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Aluno a = new Aluno();
+
+            Console.Write("Digite o Nome do Aluno: ");
+            a.Nome = Console.ReadLine();
+
+            Console.WriteLine("Digite as 3 notas do aluno: ");
+            a.n1 = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            a.n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            a.n3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double nf = a.NotaFinal();
+
+            Console.WriteLine("NOTA FINAL: " + 
+                nf.ToString("F2",CultureInfo.InvariantCulture));
+
+            if(nf >= 60)
+            {
+                Console.WriteLine("APROVADO");
+            }
+            else
+            {
+                Console.WriteLine("REPROVADO");
+                Console.WriteLine("FALTARAM " + (60.00 - nf) + " PONTOS");
+            }
+
+        }
+    }
+}
